e4c8f7c088906f62f71dccfe0016fe2393d0316d
Anthony91501 <90730954+Anthony91501@users.noreply.github.com>
2022-11-18 14:15:18 -0500

    changed array size

--- src/users.h
+++ src/users.h
@@ -36,7 +36,7 @@ struct user {
     float gpa;
     char address[50]; // IP Address user is connected from
     int status;
-    char password[20];
+    char password[50];
 };
 
 static struct user userList[NUM_USERS];
